56fe67412870514d79be8b01df2e552305d93a50
Interittus13 <arpit.gupta.0121@gmail.com>
2023-09-30 02:19:40 +0530

    updated vars.tf

--- env/dev/vars.tf
+++ env/dev/vars.tf
@@ -5,3 +5,27 @@ variable "rg_module" {
         tags = optional(map(string))
     }))
 }
+
+variable "vnet_module" {
+    type = map(object({
+      name = string
+      location = optional(string, "central india")
+      resource_group_name = string
+      address_space = list(string)
+      dns_servers = optional(list(string))
+      tags = optional(map(string))
+      subnets = map(object({
+        subnet_name = string
+        address_prefix = string
+      }))
+    }))
+}
+
+variable "peering_module" {
+  type = map(object({
+    resource_group_name = string
+    peering_name = string
+    virtual_network_name = string
+    remote_virtual_network_name = string
+  }))
+}
